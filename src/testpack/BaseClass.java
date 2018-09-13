f8b8de7233cca2416da2b7ad508701163a56ed55
mouli786 <43184612+mouli786@users.noreply.github.com>
2018-09-13 15:27:25 +0530

    Revert "Test Msg"

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -6,7 +6,6 @@ public class BaseClass {
 		// TODO Auto-generated method stub
 		System.out.println("this is my base class");
 		System.out.println("added some code");
-		System.out.println("This is my git task");
-	} 
+	}
 
 }
