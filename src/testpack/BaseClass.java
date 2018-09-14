cf4c2833729a686ff8565fe931a44743f76646e2
kumara30 <akumar.nan@gmail.com>
2018-09-14 14:16:02 +0530

    created stringreverse

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -6,6 +6,20 @@ public class BaseClass {
 		// TODO Auto-generated method stub
 		System.out.println("this is my base class");
 		System.out.println("added some code");
+		System.out.println("merge");
+		
+		stringrev_kumar();
+	}
+	public static void stringrev_kumar() 
+	{
+		String str = "Git working environment";
+		
+		for(int i = str.length()-1; i>=0; i--)
+		{
+			System.out.print(str.charAt(i));
+		}
+		
+		
 	}
 
 }
